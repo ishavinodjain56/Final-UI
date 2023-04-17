98457fa9e7e732389db62a777274a739c6c3ff0f
Keval Dhanani <dhanani.kev01@gmail.com>
2023-04-17 21:59:30 +0530

    Assets Page Updated

--- admin_dashboard/app.py
+++ admin_dashboard/app.py
@@ -11,6 +11,10 @@ def index():
 def activitiesPage():
     return render_template('activities.html')
 
+@app.route('/assets')
+def assetsPage():
+    return render_template('assets.html')
+
 if __name__ == '__main__':
     port = 5000
     app.run(host='0.0.0.0', port=port, debug=True)
