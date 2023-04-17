2105ad96d02a43ea4530383acc01f0a0b2d43901
Keval Dhanani <dhanani.kev01@gmail.com>
2023-04-17 21:36:17 +0530

    Changed Folder Structure for Flask and app.py added

---
+++ admin_dashboard/app.py
@@ -0,0 +1,16 @@
+from flask import *
+
+app = Flask(__name__)
+app.secret_key = b'final-ui/admin_dashboard@secret007'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/activities')
+def activitiesPage():
+    return render_template('activities.html')
+
+if __name__ == '__main__':
+    port = 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
