0c1a9444a37ecb4e40730034b034ff02a1a84c13
paramdoshi <param.d@somaiya.edu>
2023-04-29 23:54:23 +0530

    Updated on 29-4-23

--- admin_dashboard/app.py
+++ admin_dashboard/app.py
@@ -2,23 +2,10 @@ from flask import *
 from flask_mysqldb import MySQL
 from datetime import datetime
 from bs4 import BeautifulSoup as bs
+import pytz
 import json
 import os
-
-# from flask import Flask, redirect, request, url_for
-# from flask_login import (
-#     LoginManager,
-#     current_user,
-#     login_required,
-#     login_user,
-#     logout_user,
-# )
-# from oauthlib.oauth2 import WebApplicationClient
-# import requests
-
-# # Internal imports
-# from db import init_db_command
-# from user import User
+import base64
 
 app = Flask(__name__)
 app.secret_key = b'final-ui/admin_dashboard@secret007'
@@ -128,11 +115,69 @@ def notificationsPage():
 
 @app.route('/profile')
 def profilePage():
-    return render_template('profile.html')
+    if "curr_user" in session:
+        eprofile = session['curr_user']
+        cur = mysql.connection.cursor()
+        getprofile = cur.execute("SELECT * FROM profile_info where email='"+eprofile+"' ")
+        if getprofile>0:
+            profiledetails = cur.fetchall()
+            print(profiledetails[0][1])
+            encoded_image = base64.b64encode(profiledetails[0][1]).decode('utf-8')
+            print(encoded_image)
+            return render_template('profile.html',profiledetails=profiledetails,encoded_image=encoded_image)
+    
+    return redirect('/login')
 
 @app.route('/editprofile')
 def editprofilePage():
     return render_template('edit-profile.html')
+
+@app.route('/addprofileinfo',methods=['GET','POST'])
+def addprofile():
+    if request.method == 'POST':
+        pimg = request.files['img']
+        firstname = request.form['FN']
+        lastname = request.form['LN']
+        bd = request.form['BD']
+        birthdate = datetime.strptime(bd,'%d/%m/%Y')
+
+        gender = request.form['gender']
+        address = request.form['address']
+        country = request.form['country']
+        state = request.form['state']
+        pin = request.form['pin']
+        mobile = request.form['mobile']
+        email = request.form['email']
+        institution = request.form['institution']
+        degree = request.form['degree']
+        sd = request.form['SD']
+        startingdate = datetime.strptime(sd,'%d/%m/%Y')
+        cd = request.form['CD']
+        endingdate = datetime.strptime(cd, '%d/%m/%Y')
+        grade = request.form['grade']
+        skills= request.form['skills']
+        achievements = request.form['achievements']
+        # file = request.form['files']
+        
+        cur = mysql.connection.cursor()
+        # cur.execute("ALTER TABLE tickets AUTO_INCREMENT=1")
+        cur.execute("INSERT INTO profile_info VALUES (NULL,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (pimg,firstname,lastname,birthdate,gender,address,country,state,pin,mobile,email,institution,degree,startingdate,endingdate,grade,skills,achievements))
+
+        mysql.connection.commit()
+
+        cur.close()
+    return redirect('/profile')
+
+@app.route('/loginsuccess',methods=['GET','POST'])
+def loginsuccess():
+    if request.method == 'POST':
+        eid = json.loads(request.data)
+        print(eid['user_email'])
+        session["curr_user"] = eid['user_email']
+        return redirect('/dashboard')
+    
+    return ""
+
 
 @app.template_filter('strftime')
 def _jinja2_filter_datetime(date, fmt=None):
@@ -141,6 +186,14 @@ def _jinja2_filter_datetime(date, fmt=None):
     native = date.replace(tzinfo=None)
     format = fmt or '%b %d, %Y'
     return native.strftime(format)
+
+@app.template_filter('strptime')
+def _jinja2_filter_datetime(date, fmt):
+    if isinstance(date, str):
+        date = datetime.strptime(date, '%d/%m/%Y')
+    # native = date.replace(tzinfo=None)
+    format = fmt or '%b %d, %Y'
+    return date.strftime(format)
 
 if __name__ == '__main__':
     app.run(debug=True)
